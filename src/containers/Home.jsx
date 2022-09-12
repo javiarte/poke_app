c3c023bb22e87d3499e85c7357a2ca3abb8ff768
javiarte <33763596+javiarte@users.noreply.github.com>
2022-09-12 03:54:33 -0500

    terminando home y agregando archivos css

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Pokemons from '../components/Pokemons';
+import  { getPokemons, getPokemonData } from '../apiState'
+
 const Home = () => {
-    return(
-        <h2>Aqui va la magia</h2>
+    const [pokemons, setPokemons] = useState([])
+        
+    setTimeout(() => {
+        this.setState({
+            items: this.state.items.concat(Array.from({ length: 20 }))
+        });
+    }, 1500);
+
+    const fetchPokemons = async () => {
+        try {
+            const data = await getPokemons()
+            const promises = data.results.map(async (pokemon) => {
+                return await getPokemonData(pokemon.url)
+            })
+            const results = await Promise.all(promises)
+            setPokemons(results)
+        } catch (err) {}
+    }
+
+    useEffect(() => {
+        fetchPokemons()
+    }, [])
+
+    return (
+        <div>
+            <Pokemons pokemons={pokemons} />
+        </div>
     )
 }
 
